988b4f3386824ce93e9f6f4d3d8d6d5252d045b3
BuildTools <unconfigured@null.spigotmc.org>
2023-06-19 16:52:17 +0200

    change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print(12345);
+        System.out.print("If you see this message, you didn't screw up installing JDK");
     }
 }
