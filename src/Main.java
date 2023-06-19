028889718bd962f2000d9290a6e27c08bc31a77a
Luis <luis.weitl1411@gmail.com>
2023-06-19 17:19:38 +0200

    change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("If you see this message, you didn't screw up installing JDK!");
+        System.out.print("If you see this message, you didn't screw up installing your precious JDK!");
     }
 }
